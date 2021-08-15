ce5cdf6aa2b0b4fa4d8ce47ee9221d3803fc2637
breezedeus <breezedeus@163.com>
2021-08-15 19:58:56 +0800

    upgrade: support ICPR-MTWI-2018 and ICDAR-RCTW-2017 datasets

--- scripts/generate_idx_file.py
+++ scripts/generate_idx_file.py
@@ -2,7 +2,9 @@ import os
 import glob
 
 
-def generate_idx_pairs(img_dir, img_prefix_dir, label_prefix_dir, label_prefix_fn=''):
+def generate_icdar2015_idx_pairs(
+    img_dir, img_prefix_dir, label_prefix_dir, label_prefix_fn=''
+):
     imglst = glob.glob1(img_dir, '*g')
     imglst.sort()
     idx_pairs = []
@@ -16,20 +18,60 @@ def generate_idx_pairs(img_dir, img_prefix_dir, label_prefix_dir, label_prefix_f
     return idx_pairs
 
 
+def generate_icpr_mtwi_2018_idx_pairs(img_dir, img_prefix_dir, label_prefix_dir):
+    imglst = glob.glob1(img_dir, '*g')
+    imglst.sort()
+    idx_pairs = []
+    for img in imglst:
+        img_fp = os.path.join(img_prefix_dir, img)
+        name = img.rsplit('.', maxsplit=1)[0]
+        label_fp = os.path.join(label_prefix_dir, name + '.txt')
+        idx_pairs.append((img_fp, label_fp))
+
+    print(f'{len(idx_pairs)} pairs are generated')
+    return idx_pairs
+
+
 def save_idx_file(idx_pairs, output_fp):
     with open(output_fp, 'w') as f:
         for one_pair in idx_pairs:
             f.write('\t'.join(one_pair) + '\n')
 
 
-def main():
+def icdar2015():
     img_dir = 'data/icdar2015/train/images'
     img_prefix_dir = 'icdar2015/train/images'
     label_prefix_dir = 'icdar2015/train/gts'
     label_prefix_fn = 'gt_'
-    idx_pairs = generate_idx_pairs(img_dir, img_prefix_dir, label_prefix_dir, label_prefix_fn)
+    idx_pairs = generate_icdar2015_idx_pairs(
+        img_dir, img_prefix_dir, label_prefix_dir, label_prefix_fn
+    )
     save_idx_file(idx_pairs, 'data/icdar2015/train.tsv')
 
 
+def icpr_mtwi_2018():
+    for i in ('1000', '9000'):
+        img_dir = '/home/ein/jinlong/std_data/ICPR-MTWI-2018/train/image_%s' % i
+        img_prefix_dir = 'ICPR-MTWI-2018/train/image_%s' % i
+        label_prefix_dir = 'ICPR-MTWI-2018/train/txt_%s' % i
+        idx_pairs = generate_icpr_mtwi_2018_idx_pairs(
+            img_dir, img_prefix_dir, label_prefix_dir
+        )
+        save_idx_file(
+            idx_pairs, '/home/ein/jinlong/std_data/ICPR-MTWI-2018/train_%s.tsv' % i
+        )
+
+
+def icdar_rctw_2017():
+    img_dir = '/home/ein/jinlong/std_data/ICDAR-RCTW-2017/train_images'
+    img_prefix_dir = 'ICDAR-RCTW-2017/train_images'
+    label_prefix_dir = 'ICDAR-RCTW-2017/train_gts'
+    idx_pairs = generate_icpr_mtwi_2018_idx_pairs(
+        img_dir, img_prefix_dir, label_prefix_dir
+    )
+    save_idx_file(idx_pairs, '/home/ein/jinlong/std_data/ICDAR-RCTW-2017/train.tsv')
+
+
 if __name__ == '__main__':
-    main()
+    icpr_mtwi_2018()
+    icdar_rctw_2017()
