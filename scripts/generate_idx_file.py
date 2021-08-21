e483ef8f098f8b801c3eb04c19799de4f2514e7a
breezedeus <breezedeus@163.com>
2021-08-21 11:06:38 +0800

    support to process icdar_2019_lstv dataset

--- scripts/generate_idx_file.py
+++ scripts/generate_idx_file.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import json
+from itertools import chain
 
 
 def generate_icdar2015_idx_pairs(
@@ -72,6 +74,54 @@ def icdar_rctw_2017():
     save_idx_file(idx_pairs, '/home/ein/jinlong/std_data/ICDAR-RCTW-2017/train.tsv')
 
 
+def generate_icpr_2019_lstv_idx_pairs(
+    label_json_fp, out_label_dir, img_prefix_dir, label_prefix_dir
+):
+    idx_pairs = []
+    labels = json.load(open(label_json_fp))
+    for fname, info_list in labels.items():
+        new_info = []
+        for box_info in info_list:
+            if len(box_info['points']) != 4:
+                continue
+            box = list(chain(*box_info['points']))
+
+            text = box_info['transcription']
+            if box_info['illegibility']:
+                text = '###'
+
+            box.append(text)
+            new_info.append(list(map(str, box)))
+
+        if new_info:
+            label_fn = f'{fname}.txt'
+            label_fp = os.path.join(out_label_dir, fname)
+            with open(label_fp, 'w') as f:
+                for line in new_info:
+                    f.write(','.join(line) + '\n')
+
+            img_fn = f'{fname}.jpg'
+            img_fp = os.path.join(img_prefix_dir, img_fn)
+            label_fp = os.path.join(label_prefix_dir, label_fn)
+            idx_pairs.append((img_fp, label_fp))
+    return idx_pairs
+
+
+def icdar_2019_lstv():
+    label_json_fp = '/home/ein/jinlong/std_data/ICDAR2019-LSTV/train_full_labels.json'
+    label_dir = '/home/ein/jinlong/std_data/ICDAR2019-LSTV/train_gts'
+    img_prefix_dir = 'ICDAR2019-LSTV/train_images'
+    label_prefix_dir = 'ICDAR2019-LSTV/train_gts'
+    if not os.path.exists(label_dir):
+        os.makedirs(label_dir)
+
+    idx_pairs = generate_icpr_2019_lstv_idx_pairs(
+        label_json_fp, label_dir, img_prefix_dir, label_prefix_dir
+    )
+    save_idx_file(idx_pairs, '/home/ein/jinlong/std_data/ICDAR2019-LSTV/train.tsv')
+
+
 if __name__ == '__main__':
-    icpr_mtwi_2018()
-    icdar_rctw_2017()
+    # icpr_mtwi_2018()
+    # icdar_rctw_2017()
+    icdar_2019_lstv()
