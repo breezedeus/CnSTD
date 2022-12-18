6911ad7e59d75e4a14201bab04355b37e8d4fb4e
breezedeus <breezedeus@163.com>
2022-12-18 23:00:48 +0800

    fix: out-of-bounds issue with rectangle box

--- cnstd/yolov7/plots.py
+++ cnstd/yolov7/plots.py
@@ -77,14 +77,18 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
     # Plots one bounding box on image img
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    c1, c2 = [int(x[0]), int(x[1])], [int(x[2]), int(x[3])]
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        c2 = [c1[0] + t_size[0], c1[1] - t_size[1] - 3]
+        txt_b_l = [c1[0], c1[1] - 2]
+        if c2[1] < 0:  # 出界了
+            c2[1] = c1[1] + t_size[1] + 3
+            txt_b_l[1] = c1[1] + t_size[1] + 1
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.putText(img, label, txt_b_l, 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
 def plot_one_box_PIL(box, img, color=None, label=None, line_thickness=None):
